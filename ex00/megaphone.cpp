89dbfde09a1e3191ef4cc39fa732cecc0a71211f
gltats <tatianasofiagomes831@gmail.com>
2023-10-07 20:48:29 +0000

    first exercise

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgomes-l <tgomes-l@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 19:56:36 by tgomes-l          #+#    #+#             */
+/*   Updated: 2023/10/07 20:44:07 by tgomes-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cctype>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if(argc >= 2)
+    {
+        for(int i = 1; argv[i]; i++)
+        {
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+                std::cout << " ";               
+        }
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
+    return(0);
+}
